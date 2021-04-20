9dbdf5fe7f465ed5155284ae5eddd48cfc837eee
HP <pdevs18@hotmail.com>
2021-04-20 17:15:37 +0530

    added touch gestures

--- script.js
+++ script.js
@@ -47,12 +47,115 @@ canvas.addEventListener('mouseout', onMouseUp, false);
 canvas.addEventListener('mousemove', onMouseMove, false);
 canvas.addEventListener('wheel', onMouseWheel, false);
 
-//canvas.addEventListener('touchstart', onTouchStart);
-//canvas.addEventListener('touchend', onTouchEnd);
-//canvas.addEventListener('touchcancle', onTouchEnd);
-//canvas.addEventListener('touchmove', onTouchMove);
 
+canvas.addEventListener('touchstart', onTouchStart);
+canvas.addEventListener('touchend', onTouchEnd);
+canvas.addEventListener('touchcancle', onTouchEnd);
+canvas.addEventListener('touchmove', onTouchMove);
 
+//touch funcntions
+window.ontouchstart = function(event) {
+     //If there is more than one touch
+        event.preventDefault();
+    
+}
+const prevTouches = [null, null]; // up to 2 touches
+        let singleTouch = false;
+        let doubleTouch = false;
+        function onTouchStart(event) {
+            if (event.touches.length == 1) {
+                singleTouch = true;
+                doubleTouch = false;
+            }
+            if (event.touches.length >= 2) {
+                singleTouch = false;
+                doubleTouch = true;
+            }
+
+            // store the last touches
+            prevTouches[0] = event.touches[0];
+            prevTouches[1] = event.touches[1];
+
+        }
+        function onTouchMove(event) {
+            // get first touch coordinates
+            const touch0X = event.touches[0].pageX;
+            const touch0Y = event.touches[0].pageY;
+            const prevTouch0X = prevTouches[0].pageX;
+            const prevTouch0Y = prevTouches[0].pageY;
+
+            const scaledX = toTrueX(touch0X);
+            const scaledY = toTrueY(touch0Y);
+            const prevScaledX = toTrueX(prevTouch0X);
+            const prevScaledY = toTrueY(prevTouch0Y);
+
+            if (singleTouch) {
+                // add to history
+                drawings.push({
+                    x0: prevScaledX,
+                    y0: prevScaledY,
+                    x1: scaledX,
+                    y1: scaledY
+                })
+                drawLine(prevTouch0X, prevTouch0Y, touch0X, touch0Y);
+            }
+
+            if (doubleTouch) {
+                // get second touch coordinates
+                const touch1X = event.touches[1].pageX;
+                const touch1Y = event.touches[1].pageY;
+                const prevTouch1X = prevTouches[1].pageX;
+                const prevTouch1Y = prevTouches[1].pageY;
+
+                // get midpoints
+                const midX = (touch0X + touch1X) / 2;
+                const midY = (touch0Y + touch1Y) / 2;
+                const prevMidX = (prevTouch0X + prevTouch1X) / 2;
+                const prevMidY = (prevTouch0Y + prevTouch1Y) / 2;
+
+                // calculate the distances between the touches
+                const hypot = Math.sqrt(Math.pow((touch0X - touch1X), 2) + Math.pow((touch0Y - touch1Y), 2));
+                const prevHypot = Math.sqrt(Math.pow((prevTouch0X - prevTouch1X), 2) + Math.pow((prevTouch0Y - prevTouch1Y), 2));
+
+                // calculate the screen scale change
+                var zoomAmount = hypot / prevHypot;
+                scale = scale * zoomAmount;
+                const scaleAmount = 1 - zoomAmount;
+
+                // calculate how many pixels the midpoints have moved in the x and y direction
+                const panX = midX - prevMidX;
+                const panY = midY - prevMidY;
+                // scale this movement based on the zoom level
+                offsetX += (panX / scale);
+                offsetY += (panY / scale);
+
+                // Get the relative position of the middle of the zoom.
+                // 0, 0 would be top left. 
+                // 0, 1 would be top right etc.
+                var zoomRatioX = midX / canvas.clientWidth;
+                var zoomRatioY = midY / canvas.clientHeight;
+
+                // calculate the amounts zoomed from each edge of the screen
+                const unitsZoomedX = trueWidth() * scaleAmount;
+                const unitsZoomedY = trueHeight() * scaleAmount;
+
+                const unitsAddLeft = unitsZoomedX * zoomRatioX;
+                const unitsAddTop = unitsZoomedY * zoomRatioY;
+
+                offsetX += unitsAddLeft;
+                offsetY += unitsAddTop;
+
+                redrawCanvas();
+            }
+            prevTouches[0] = event.touches[0];
+            prevTouches[1] = event.touches[1];
+        }
+        function onTouchEnd(event) {
+            singleTouch = false;
+            doubleTouch = false;
+        }
+
+//moouse fucnctions
 let leftMouseDown = false;
 let rightMousedown = false;
 function onMouseDown(event){
@@ -116,7 +219,7 @@ function onMouseWheel(event){
     var distX = event.pageX / canvas.clientWidth;
     var distY  = event.pageY / canvas.clientHeight;
 
-    const unitsZoomedX = truewidth() * scaleAmount;
+    const unitsZoomedX = trueWidth() * scaleAmount;
     const unitsZoomedY = trueHeight() * scaleAmount
 
     const unitsAddLeft = unitsZoomedX * distX;
@@ -199,7 +302,7 @@ function trueHeight(){
     return canvas.clientHeight / scale;
 }
 
-function truewidth(){
+function trueWidth(){
     return canvas.clientWidth/scale;
 }
 
